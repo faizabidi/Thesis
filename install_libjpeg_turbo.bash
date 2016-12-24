50c4712fc23ebf25bc929e797ddb332413799459
FaizOnCube <fabidi89@vt.edu>
2016-12-24 00:39:32 -0500

    Corrected % with $.

--- install_libjpeg_turbo.bash
+++ install_libjpeg_turbo.bash
@@ -11,45 +11,61 @@ cd /home/faiz89/git
 
 # Check if autoconf is installed
 AUTOCONF="$(locate bin/autoconf)"
-if [ -z "%AUTOCONF" ]; then
+if [ -z "$AUTOCONF" ]; then
     set -ex # Exit error and echo the command
+    echo "Installing autoconf..."
     sudo yum install autoconf -y
     set +ex # Unset the exit error and the echo command
+else
+    echo "autoconf already installed. Not re-installing...."
 fi
 
 # Check if automake is installed
 AUTOMAKE="$(locate bin/automake)"
-if [ -z "%AUTOMAKE" ]; then
+if [ -z "$AUTOMAKE" ]; then
     set -ex
+    echo "Installing automake..."
     sudo yum install automake -y
     set +ex
+else
+    echo "automake already installed. Not re-installing..."
 fi
 
 # Check if yasm or nasm installed
 NASM="$(locate bin/nasm)"
 YASM="$(locate bin/yasm)"
-if [ -z "%NASM" ] && [ -z "%YASM" ]; then
+if [ -z "$NASM" ] && [ -z "$YASM" ]; then
     set -ex
+    echo "Installing yasm...."
     sudo yum install yasm -y
     set +ex 
+else
+    echo "yasm or nasm or both already installed. Not re-installing..."
 fi
 
 # Check if JDK or openjdk is installed
 JDK="$(java -version 2>&1 | grep jdk)"
-if [ -z "%JDK" ]; then
+if [ -z "$JDK" ]; then
     set -ex
+    echo "Installing openjdk version 1.8.0...."
     sudo yum install java-1.8.0-openjdk -y
     sudo yum install java-1.8.0-openjdk-devel -y
     set +ex
+else
+    echo "JDK already installed. Not re-installing...."
 fi
 
 # Check if there already is a git repo downloaded in ~/git
-LIBJPEG="$(ls | grep libjpeg-turbo)"
-if [ -z "%LIBJPEG" ]; then
+LIBJPEG="$(ls /home/faiz89/git | grep libjpeg-turbo)"
+if [ -z "$LIBJPEG" ]; then
     set -ex
+    echo "Cloning libjpeg-turbo from GitHub....."
     # Clone the libjpeg-turbo SDK repo from GitHub
     git clone https://github.com/libjpeg-turbo/libjpeg-turbo.git
     set +ex
+else
+    echo "There is already a GitHub repo for libjpeg-turbo downloaded in
+    /home/faiz89/git. Not re-downloading..."
 fi
 
 set -ex
