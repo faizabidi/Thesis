02c7f9263437018ed087e4e570f8b665ff9a5da7
FaizOnCube <fabidi89@vt.edu>
2016-12-31 00:05:30 -0500

    Added support to provide name/IP of the VNCSERVER to connect to.

--- vnc_operations/start_vncviewrers.bash
+++ vnc_operations/start_vncviewrers.bash
@@ -1,7 +1,13 @@
 #!/bin/bash
+if [ $# -ne 1 ]; then
+    echo "Enter the VNCSERVER name/IP."
+    exit 1
+fi
+
+SERVER=$1
 
 for i in {0..14..2};do
     j=$((($i/2)+1))
-    DISPLAY=:0.$i vncviewer -fullscreen -passwd passwd console.sv.vt.edu:$j &
+    DISPLAY=:0.$i vncviewer -fullscreen -passwd passwd $SERVER:$j &
 done
 
