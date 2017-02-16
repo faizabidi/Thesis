f8b278434de640b06eb8853650ccdbe21fb31d7c
Faiz Abbas Abidi <fabidi89@nrlogin3.cluster>
2017-02-16 12:12:17 -0500

    Just start the pvserver with the right flags.

---
+++ gen_perations/start_pvserver.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Kill any existing running pvservers
+pkill pvserver
+
+# Start 8 new pvservers
+mpiexec -np 8 \
+	vglrun pvserver \
+	/home/fabidi89/git/Thesis/vnc_operations/cave-momo.pvx &
