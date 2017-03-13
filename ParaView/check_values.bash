fb4a6dba0ae41a9c9a8a093d21486a4dd8b1639d
FaizOnCube <fabidi89@vt.edu>
2017-03-12 22:01:39 -0400

    Added logic to move the temp file back to the original name.

--- ParaView/check_values.bash
+++ ParaView/check_values.bash
@@ -2,5 +2,9 @@
 
 FILENAME=$1
 
-awk '$1 + 0 == $1 && $2 + 0 == $2 && $3 + 0 == $3 && $4 + 0 == $4' $FILENAME
+# Make sure all values are float
+awk '$1 + 0 == $1 && $2 + 0 == $2 && $3 + 0 == $3 && $4 + 0 == $4' $FILENAME > temp
+
+# Move the temp file back to its parent name
+mv temp  $FILENAME
 
