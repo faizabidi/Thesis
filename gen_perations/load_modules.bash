fa85457fdb2bfd953baa98c2978787538a32deb9
Faiz Abbas Abidi <fabidi89@nrlogin3.cluster>
2017-03-28 20:46:27 -0400

    Don't remember what I did. But it's important.

--- gen_perations/load_modules.bash
+++ gen_perations/load_modules.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#. $MODULESHOME/init/bash
-#source /etc/profile.d/vt-modules.sh
-module load gcc/5.2.0 mpich/3.1.4 qt python 
-#module load ParaView/5.0.1
+module load gcc/5.2.0 mpich/3.1.4  qt python 
 module load virtualgl
 module load turbovnc
+#module load cmake
+#module load perl/5.20.2
+
--- gen_perations/start_pvserver.bash
+++ gen_perations/start_pvserver.bash
@@ -6,4 +6,4 @@
 # Start 8 new pvservers
 mpiexec -np 8 \
 	vglrun pvserver \
-	/home/fabidi89/git/Thesis/vnc_operations/cave-momo.pvx &
+	/home/fabidi89/git/Thesis/vnc_operations/cave-momo.pvx
