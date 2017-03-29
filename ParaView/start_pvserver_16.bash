2af47c8a3623bbf0fb9d0e10ab6b4fdcb0ffe0b1
Faiz Abbas Abidi <fabidi89@nrlogin3.cluster>
2017-03-28 20:45:45 -0400

    Starting pvserver.

---
+++ ParaView/start_pvserver_16.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mpiexec -np 16 vglrun /home/fabidi89/Paraview-v5.2.0/bin/pvserver ~/git/Thesis/vnc_operations/cave-mono-16displays.pvx
---
+++ ParaView/start_pvserver_8.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mpiexec -np 8 vglrun /home/fabidi89/Paraview-v5.2.0/bin/pvserver ~/git/Thesis/vnc_operations/cave-momo.pvx
