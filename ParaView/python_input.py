12e331f25bf17e5e830fdbc34f339512e69f6f05
Faiz Abidi <faiz89@console.sv.vt.edu>
2017-04-04 12:53:34 -0400

    Added scroll to mouse movements.

--- ParaView/python_input.py
+++ ParaView/python_input.py
@@ -1,11 +1,134 @@
 # https://pypi.python.org/pypi/pynput
 
+import time
 from pynput.mouse import Button, Controller
 
 mouse = Controller()
-
-mouse.position = (572,34)
+##############################
+mouse.scroll(1, 2)
+mouse.scroll(1, 2)
+mouse.scroll(1, 2)
+mouse.scroll(1, 2)
+mouse.scroll(1, 2)
+##############################
+mouse.position = (872, 544)
 mouse.press(Button.left)
-mouse.move(2911,29)
+mouse.move(-100, 544)
 mouse.release(Button.left)
-mouse.position = (2911,29)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(200, 544)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(200, 844)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(-500, 844)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(1, 0)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(350, 350)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(-350, -350)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(100, 100)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(-273, -273)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(-350, 675)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(-675, -555)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(-350, -505)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(675, -675)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(2, 200)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(80, 90)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(67, -30)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(-35, -350)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(78, 369)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(65, 65)
+mouse.release(Button.left)
+time.sleep(1)
+##############################
+mouse.position = (872, 544)
+mouse.press(Button.left)
+mouse.move(45, -350)
+mouse.release(Button.left)
+time.sleep(1)
+
+
