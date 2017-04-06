dbc538f985427f74e90787bc224bf5df95146914
FaizOnCube <fabidi89@vt.edu>
2017-04-06 17:19:18 -0400

    Added options for vncviewer.

--- vnc_operations/start_vncviewrers_8.bash
+++ vnc_operations/start_vncviewrers_8.bash
@@ -1,16 +1,22 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-    echo "Enter the VNCSERVER name/IP."
+if [ $# -ne 7 ]; then
+    echo "Enter encoding type(use Tight), jpeg (0 or 1), quality(0 to 100), compression level(0 to 9), and the the VNCSERVER name/IP."
     exit 1
 fi
+
+ENCODING=$1
+JPEG=$2
+QUALITY=$3
+SUBSAMPLING=$4
+COMPRESS_LEVEL=$5
+SERVER=$6
+FULLSCREEN=$7
 
 # Kill all existing vncviewers
 ps -ef | grep VncViewer | awk '{print $2}' | xargs kill
 
-SERVER=$1
-
 for i in {0..14..2};do
     j=$((($i/2)+1))
-    DISPLAY=:0.$i vncviewer -fullscreen -passwd passwd $SERVER:$j &
+    DISPLAY=:0.$i vncviewer -fullscreen=$7 -passwd=passwd -encoding $ENCODING -jpeg=$JPEG -quality $QUALITY -subsampling $SUBSAMPLING -compresslevel $COMPRESS_LEVEL $SERVER:$j &
 done
 
