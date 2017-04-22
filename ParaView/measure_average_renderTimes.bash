ec734de7c185d5c35da4193d29cf79483000ac9e
FaizOnCube <fabidi89@vt.edu>
2017-04-22 15:03:31 -0400

    Measure still and interactive render times.

--- ParaView/measure_average_renderTimes.bash
+++ ParaView/measure_average_renderTimes.bash
@@ -3,3 +3,4 @@
 FILE=$1
 
 cat $FILE | grep "Still Render" | awk '{sum += $3; n++} END { if (n > 0) print sum / n; }'
+cat $FILE | grep "Interactive Render" | awk '{sum += $3; n++} END { if (n > 0) print sum / n; }'
